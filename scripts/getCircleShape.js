53b9f1fed95d6a1b0e37fa3916e110809c26e7c3
Michael Enion <caewok_2000@yahoo.com>
2021-12-05 13:21:26 -0800

    Use ClockwiseSweep to create a walled circle template
    Patch MeasuredTemplate.prototype._getCircleShape.
    Shift the polygon to 0,0 origin.

---
+++ scripts/getCircleShape.js
@@ -0,0 +1,45 @@
+/* globals
+*/
+
+'use strict';
+
+import { log } from "./module.js";
+
+export function walledTemplateGetCircleShape(wrapped, distance) {
+  // origin is this.data.x, this.data.y
+  // shape is relative to the origin
+  
+  const circle = wrapped(distance);
+  log(`getCircleShape origin ${this.data.x}, ${this.data.y} with distance ${distance}`, circle);
+  
+  //   return circle;
+  // get a polygon from the canvas
+  const poly = new ClockwiseSweepPolygon();
+  poly.initialize({x: this.data.x, y: this.data.y}, { 
+    angle: 360,
+    debug: false,
+    density: 60,
+    radius: distance,
+    rotation: 0,
+    type: "light"
+  });
+    
+  poly.compute();
+  
+
+  // need to shift the polygon to have 0,0 origin because of how the MeasuredTemplate 
+  // sets the origin for the drawing separately
+  // Polygon points, annoyingly, are array [x0, y0, x1, y1, ...]
+  const ln = poly.points.length;
+  for(let i = 0; i < ln; i += 2) {
+    poly.points[i] = poly.points[i] - this.data.x;
+    poly.points[i + 1] = poly.points[i + 1] - this.data.y;
+  }
+  
+  //log(`getCircleShape poly points`, poly.points);
+  
+  return poly;
+  //return new PIXI.Polygon(poly.points);
+}
+
+
--- scripts/module.js
+++ scripts/module.js
@@ -4,10 +4,39 @@ Hooks
 
 'use strict';
 
+import { MODULE_ID } from "./const.js";
+import { registerWalledTemplates } from "./patching.js";
+
+/**
+ * Log message only when debug flag is enabled from DevMode module.
+ * @param {Object[]} args  Arguments passed to console.log.
+ */
+export function log(...args) {
+  try {
+    const isDebugging = game.modules.get(`_dev-mode`)?.api?.getPackageDebugValue(MODULE_ID);
+    if( isDebugging ) {
+      console.log(MODULE_ID, `|`, ...args);
+    }
+  } catch (e) { 
+    // empty 
+  }
+}
+
+/**
+ * Tell DevMode that we want a flag for debugging this module.
+ * https://github.com/League-of-Foundry-Developers/foundryvtt-devMode
+ */
+Hooks.once(`devModeReady`, ({ registerPackageDebugFlag }) => {
+  registerPackageDebugFlag(MODULE_ID);
+});
+
 Hooks.once('init', async function() {
+  log(`Initializing...`);
+  registerWalledTemplates();
+  
+  game.modules.get(MODULE_ID).api = {
+   
+  }
 
 });
 
-Hooks.once('ready', async function() {
-
-});
