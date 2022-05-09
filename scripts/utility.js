de6abeeb52efe166c3f75d1cda1989d2110305eb
Michael Enion <caewok_2000@yahoo.com>
2022-05-09 16:39:53 -0700

    Undo the origin offset for cones after building the polygon

--- scripts/utility.js
+++ scripts/utility.js
@@ -14,16 +14,3 @@ export function pointsAlmostEqual(p0, p1, e = 1e-8) {
   return p0.x.almostEqual(p1.x, e) && p0.y.almostEqual(p1.y, e);
 }
 
-
-/**
- * Same as Ray.fromAngle but returns a point instead of constructing the full Ray.
- * @param {Point}   origin    Starting point.
- * @param {Number}  radians   Angle to move from the starting point.
- * @param {Number}  distance  Distance to travel from the starting point.
- * @return {Point}  Coordinates of point that lies distance away from origin along angle.
- */
-export function pointFromAngle(origin, radians, distance) {
-  const dx = Math.cos(radians);
-  const dy = Math.sin(radians);
-  return { x: origin.x + (dx * distance), y: origin.y + (dy * distance) };
-}
