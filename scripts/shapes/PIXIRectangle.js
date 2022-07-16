e9a5590eedf4d4bc7a7d364c8c45fc6035e7fac5
Michael Enion <caewok_2000@yahoo.com>
2022-07-16 14:51:08 -0700

    Add overlaps and area methods to PIXI.Rectangle

--- scripts/shapes/PIXIRectangle.js
+++ scripts/shapes/PIXIRectangle.js
@@ -14,10 +14,115 @@ function translate(dx, dy) {
   return new this.constructor(this.x + dx, this.y + dy, this.width, this.height);
 }
 
+/**
+ * Does this rectangle overlap something else?
+ * @param {PIXI.Rectangle|PIXI.Circle|PIXI.Polygon} shape
+ * @returns {boolean}
+ */
+function overlaps(shape) {
+  if ( shape instanceof PIXI.Polygon ) { return this._overlapsPolygon(shape); }
+  if ( shape instanceof PIXI.Circle ) { return this._overlapsCircle(shape); }
+  if ( shape instanceof PIXI.Rectangle ) { return this._overlapsRectangle(shape); }
+
+  console.warn("overlaps|shape not recognized.", shape);
+  return false;
+}
+
+/**
+ * Does this rectangle overlap another?
+ * @param {PIXI.Rectangle} other
+ * @return {Boolean}
+ */
+function overlapsRectangle(other) {
+  // https://www.geeksforgeeks.org/find-two-rectangles-overlap
+  // One rectangle is completely above the other
+  if ( this.top > other.bottom || other.top > this.bottom ) return false;
+
+  // One rectangle is completely to the left of the other
+  if ( this.left > other.right || other.left > this.right ) return false;
+
+  return true;
+}
+
+/**
+ * Does this rectangle overlap a circle?
+ * @param {PIXI.Circle} circle
+ * @return {Boolean}
+ */
+function overlapsCircle(circle) {
+  // https://www.geeksforgeeks.org/check-if-any-point-overlaps-the-given-circle-and-rectangle
+  // {xn,yn} is the nearest point on the rectangle to the circle center
+  const xn = Math.max(this.right, Math.min(circle.x, this.left));
+  const yn = Math.max(this.top, Math.min(circle.y, this.bottom));
+
+  // Find the distance between the nearest point and the center of the circle
+  const dx = xn - circle.x;
+  const dy = yn - circle.y;
+  return (Math.pow(dx, 2) + Math.pow(dy, 2)) <= Math.pow(circle.radius, 2);
+}
+
+/**
+ * Does this rectangle overlap a polygon?
+ * @param {PIXI.Polygon} poly
+ * @return {Boolean}
+ */
+function overlapsPolygon(poly) {
+  if ( poly.contains(this.left, this.top)
+    || poly.contains(this.right, this.top)
+    || poly.contains(this.left, this.bottom)
+    || poly.contains(this.right, this.bottom)) { return true; }
+
+  for ( const edge of poly.iterateEdges() ) {
+    if ( this.lineSegmentIntersects(edge.A, edge.B)
+      || this.containsPoint(edge.A)
+      || this.containsPoint(edge.B)) { return true; }
+  }
+  return false;
+}
+
+/**
+ * Calculate rectangle area
+ * @return {Number}
+ */
+function area() {
+  return this.width * this.height;
+}
+
+
 // ----------------  ADD METHODS TO THE PIXI.RECTANGLE PROTOTYPE ------------------------
 export function registerPIXIRectangleMethods() {
   Object.defineProperty(PIXI.Rectangle.prototype, "translate", {
     value: translate,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(PIXI.Rectangle.prototype, "overlaps", {
+    value: overlaps,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(PIXI.Rectangle.prototype, "_overlapsRectangle", {
+    value: overlapsRectangle,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(PIXI.Rectangle.prototype, "_overlapsCircle", {
+    value: overlapsCircle,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(PIXI.Rectangle.prototype, "_overlapsPolygon", {
+    value: overlapsPolygon,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(PIXI.Rectangle.prototype, "area", {
+    value: area,
     writable: true,
     configurable: true
   });
