1a4f2b4c57ca6bc1985479bd7200ae53e67f8196
Michael Enion <caewok_2000@yahoo.com>
2023-06-01 13:07:07 -0700

    Add logic to handle the global defaults in spell items.

--- scripts/const.js
+++ scripts/const.js
@@ -16,5 +16,21 @@ export const LABELS = {
     unwalled: "walledtemplates.MeasuredTemplateConfiguration.unwalled",
     walled: "walledtemplates.MeasuredTemplateConfiguration.walled",
     recurse: "walledtemplates.MeasuredTemplateConfiguration.recurse"
-  }
+  },
+
+  WALL_RESTRICTION: {
+    light: "WALLS.Light",
+    move: "WALLS.Movement",
+    sight: "WALLS.Sight",
+    sound: "WALLS.Sound"
+  },
+
+  SPELL_TEMPLATE: {},
+
+  GLOBAL_DEFAULT: "globalDefault"
 };
+
+LABELS.SPELL_TEMPLATE.WALLS_BLOCK = duplicate(LABELS.WALLS_BLOCK);
+LABELS.SPELL_TEMPLATE.WALL_RESTRICTION = duplicate(LABELS.WALL_RESTRICTION);
+LABELS.SPELL_TEMPLATE.WALLS_BLOCK.globalDefault = "walledtemplates.MeasuredTemplateConfiguration.globalDefault";
+LABELS.SPELL_TEMPLATE.WALL_RESTRICTION.globalDefault = "walledtemplates.MeasuredTemplateConfiguration.globalDefault";
