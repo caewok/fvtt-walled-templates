8cfd22c53ba653c8edbd30f7ccb73af226aa9b4e
Michael Enion <caewok_2000@yahoo.com>
2023-06-07 05:49:58 -0700

    Move wall hooks to separate file

--- scripts/module.js
+++ scripts/module.js
@@ -37,8 +37,14 @@ import { ClockwiseSweepShape } from "./ClockwiseSweepShape.js";
 import { LightWallSweep } from "./ClockwiseSweepLightWall.js";
 import * as WalledTemplateClasses from "./WalledTemplate.js";
 
+// Hooks
+import { createWallHook, preUpdateWallHook, updateWallHook, deleteWallHook } from "./walls.js";
+
+
 // Self-executing scripts for hooks
 import "./changelog.js";
+
+// Note: Basic system hooks
 
 /**
  * Tell DevMode that we want a flag for debugging this module.
@@ -173,6 +179,9 @@ Hooks.on("getSceneControlButtons", controls => {
   });
 });
 
+// Note: Render hooks
+
+
 /**
  * Add controls to the measured template configuration
  */
@@ -205,128 +214,11 @@ function refreshMeasuredTemplateHook(template, flags) {
   if ( flags.retarget ) template.autotargetToken();
 }
 
-/**
- * Hook wall creation and update to refresh templates
- * https://foundryvtt.com/api/hookEvents.html
- * updateWall
- * createWall
- */
-
-/**
- * createWall Hook
- * @param {WallDocument} document
- * @param {Object} options { temporary: Boolean, renderSheet: Boolean, render: Boolean }
- * @param {string} userId
- */
+// Note: Wall hooks
 Hooks.on("createWall", createWallHook);
-
-function createWallHook(document, options, _userId) {
-  if (options.temporary) return;
-
-  const A = document._object.A;
-  const B = document._object.B;
-  log(`Refreshing templates on createWall ${A.x},${A.y}|${B.x},${B.y}.`);
-
-  canvas.templates.placeables.forEach(t => {
-    const bbox = t.shape.getBounds().translate(t.x, t.y);
-    if ( bbox.lineSegmentIntersects(A, B, { inside: true }) ) {
-      log(`Wall ${document.id} intersects ${t.id}`);
-      t.renderFlags.set({
-        refreshShape: true
-      });
-    }
-  });
-}
-
-/**
- * Hook for preUpdateWall, so the existing wall can be checked for whether it
- * interacts with the template
- * @param {WallDocument} document
- * @param {Object} change { c: Array[], _id: String }  Array of four coordinates plus id
- * @param {Object} options { diff: Boolean, render: Boolean }
- * @param {string} userId
- */
 Hooks.on("preUpdateWall", preUpdateWallHook);
-
-async function preUpdateWallHook(document, change, _options, _userId) {
-  const A = { x: document.c[0], y: document.c[1] };
-  const B = { x: document.c[2], y: document.c[3] };
-
-  // Issue #19: Door open/close passes a change.ds but not a change.c
-  const new_A = change.c ? { x: change.c[0], y: change.c[1] } : A;
-  const new_B = change.c ? { x: change.c[2], y: change.c[3] } : B;
-  log(`Refreshing templates on preUpdateWall ${A.x},${A.y}|${B.x},${B.y} --> ${new_A.x},${new_A.y}|${new_B.x},${new_B.y}`);
-
-  // We want to update the template if this wall is within the template, but
-  // hold until updateWall is called.
-  const promises = [];
-  canvas.templates.placeables.forEach(t => {
-    const bbox = t.shape.getBounds().translate(t.x, t.y);
-    if ( bbox.lineSegmentIntersects(A, B, { inside: true }) ) {
-      log(`Wall ${document.id} intersects ${t.id}`);
-      promises.push(t.document.setFlag(MODULE_ID, "redraw", true)); // Async
-    }
-  });
-  promises.length && ( Promise.all(promises) ); // eslint-disable-line no-unused-expressions
-}
-
-/**
- * Hook for updateWall, so the existing wall can be checked for whether it
- * interacts with the template
- * @param {WallDocument} document
- * @param {Object} change { c: Array[], _id: String }  Array of four coordinates plus id
- * @param {Object} options { diff: Boolean, render: Boolean }
- * @param {string} userId
- */
 Hooks.on("updateWall", updateWallHook);
-
-function updateWallHook(document, change, options, _userId) {
-  if (!options.diff) return;
-
-  const A = { x: document.c[0], y: document.c[1] };
-  const B = { x: document.c[2], y: document.c[3] };
-
-  canvas.templates.placeables.forEach(t => {
-    if ( t.document.getFlag(MODULE_ID, "redraw") ) {
-      t.document.setFlag(MODULE_ID, "redraw", false);
-      t.renderFlags.set({
-        refreshShape: true
-      });
-      return;
-    }
-    const bbox = t.shape.getBounds().translate(t.x, t.y);
-    if ( bbox.lineSegmentIntersects(A, B, { inside: true }) ) {
-      log(`Wall ${document.id} intersects ${t.id}`);
-      t.renderFlags.set({
-        refreshShape: true
-      });
-    }
-  });
-}
-
-/**
- * Hook for deleteWall.
- * @param {WallDocument} document
- * @param {Object} options { render: Boolean }
- * @param {string} userId
- */
 Hooks.on("deleteWall", deleteWallHook);
-
-function deleteWallHook(document, _options, _userId) {
-  const A = { x: document.c[0], y: document.c[1] };
-  const B = { x: document.c[2], y: document.c[3] };
-  log(`Refreshing templates on deleteWall ${A.x},${A.y}|${B.x},${B.y}.`);
-
-  canvas.templates.placeables.forEach(t => {
-    const bbox = t.shape.getBounds().translate(t.x, t.y);
-    if ( bbox.lineSegmentIntersects(A, B, { inside: true }) ) {
-      log(`Wall ${document.id} intersects ${t.id}`);
-      t.renderFlags.set({
-        refreshShape: true
-      });
-    }
-  });
-}
 
 /**
  * At preCreateMeasuredTemplate, attempt to get a token associated with the template.
