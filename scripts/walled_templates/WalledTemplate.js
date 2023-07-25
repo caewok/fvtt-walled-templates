36683b3f23c137a2ddb6e46afadc4ce8e3caeaa6
Michael Enion <caewok_2000@yahoo.com>
2023-07-25 12:30:01 -0700

    Use separate WalledTemplateRoundedCone file

--- scripts/walled_templates/WalledTemplate.js
+++ scripts/walled_templates/WalledTemplate.js
@@ -17,6 +17,7 @@ import { ClipperPaths } from "../geometry/ClipperPaths.js";
 import { SETTINGS, getSetting, debugPolygons } from "../settings.js";
 import { ClockwiseSweepShape, pointFromKey } from "../ClockwiseSweepShape.js";
 import { LightWallSweep } from "../ClockwiseSweepLightWall.js";
+import { WalledTemplateRoundedCone } from "./WalledTemplateRoundedCone.js";
 
 // Debugging
 import { Draw } from "../geometry/Draw.js";
@@ -774,28 +775,6 @@ export class WalledTemplateCone extends WalledTemplateRay {
     }
 
     return coneTemplates;
-  }
-}
-
-export class WalledTemplateRoundedCone extends WalledTemplateCone {
-  /**
-   * Translate the boundary shape to the correct origin.
-   * Use a circle + limited radius for the bounding shapes.
-   * @returns {[PIXI.Circle|PIXI.Rectangle|PIXI.Polygon]}
-   */
-  getTranslatedBoundaryShapes() {
-    const shape = super.getTranslatedBoundaryShapes()[0];
-    const origin = this.origin.to2d();
-    const { angle, direction } = this; // Angle is in degrees; direction is in radians.
-
-    // Use a circle + limited radius for the bounding shapes
-    const pts = shape.points;
-    const radius = Math.hypot(pts[2] - pts[0], pts[3] - pts[1]);
-    const rotation = Math.toDegrees(direction) - 90;
-
-    const circle = new PIXI.Circle(origin.x, origin.y, radius);
-    const la = new LimitedAnglePolygon(origin, {radius, angle, rotation});
-    return [circle, la];
   }
 }
 
