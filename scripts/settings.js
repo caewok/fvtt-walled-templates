6ad553bf23d9ad388d2e3684c328fb6a29d879e3
Michael Enion <caewok_2000@yahoo.com>
2023-08-03 08:17:58 -0700

    Fix circular import

--- scripts/settings.js
+++ scripts/settings.js
@@ -8,9 +8,6 @@ import { log } from "./util.js";
 import { MODULE_ID, SHAPE_KEYS } from "./const.js";
 import { registerAutotargeting } from "./patching.js";
 import { WalledTemplateShapeSettings } from "./WalledTemplateShapeSettings.js";
-import { WalledTemplateShape } from "./template_shapes/WalledTemplateShape.js";
-import { WalledTemplateCircle } from "./template_shapes/WalledTemplateCircle.js";
-import { WalledTemplateSquare } from "./template_shapes/WalledTemplateSquare.js";
 
 export const SETTINGS = {
   DEFAULT_WALLS_BLOCK: {},
@@ -56,27 +53,6 @@ for ( const shapeKey of SHAPE_KEYS ) {
   SETTINGS.DEFAULT_WALL_RESTRICTION[shapeKey] = `default-${shapeKey}-wall-restriction`;
   SETTINGS.DIAGONAL_SCALING[shapeKey] = `diagonal-scaling-${shapeKey}`;
 }
-
-// ----- NOTE: Hooks ----- //
-export const PATCHES = {};
-PATCHES.BASIC = {};
-
-/**
- * Hook updateSetting
- * @param {Setting} setting
- * @param {object} change {value, _id}
- * @param {object} opts {diff, render, type}
- * @param {string} id
- */
-function updateSettingHook(setting, _change, _opts, _id) {
-  const reg = WalledTemplateShape.shapeCodeRegister;
-  if ( setting.key === SETTINGS.DIAGONAL_SCALING.circle ) {
-    if ( setting.value ) reg.set("circle", WalledTemplateSquare);
-    else reg.set("circle", WalledTemplateCircle);
-  }
-}
-
-PATCHES.BASIC.HOOKS = { updateSetting: updateSettingHook };
 
 // ---- NOTE: Exported functions ----- //
 
