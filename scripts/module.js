e2415764644d380949f6ee0f0239e274b07822de
Michael Enion <caewok_2000@yahoo.com>
2023-09-07 07:58:30 -0700

    Change renderFlags mods to match v11.308

--- scripts/module.js
+++ scripts/module.js
@@ -95,10 +95,7 @@ Hooks.once("init", function() {
   };
 
   CONFIG.MeasuredTemplate.objectClass.RENDER_FLAGS.retarget = {};
-  CONFIG.MeasuredTemplate.objectClass.RENDER_FLAGS.refresh.propagate = ["refreshState", "refreshPosition"];
-  CONFIG.MeasuredTemplate.objectClass.RENDER_FLAGS.refreshShape.propagate = ["refreshGrid", "refreshText"];
   CONFIG.MeasuredTemplate.objectClass.RENDER_FLAGS.refreshPosition.propagate = ["retarget", "refreshShape"];
-  CONFIG.MeasuredTemplate.objectClass.RENDER_FLAGS.refreshTemplate = {};
 
   // Tell modules that the module is set up
   Hooks.callAll(`${MODULE_ID}Ready`);
